d32b433544f1fff23fa54100611b796d275c5491
Hugo Martineu <hugo.martineu@gmail.com>
2023-09-29 16:30:33 +0200

    homepage

--- src/app/components/EmailSection.tsx
+++ src/app/components/EmailSection.tsx
@@ -22,10 +22,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com">
+          <Link href="https://github.com/AchelDrinker">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="linkedin.com">
+          <Link href="www.linkedin.com/in/hugomartineu">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
