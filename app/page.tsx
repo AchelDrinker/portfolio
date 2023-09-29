82489fa0503f89262a1658f98142c42b3c813643
Hugo Martineu <hugo.martineu@gmail.com>
2023-09-29 09:37:38 +0200

    start

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen">
-      <h1 className="text-6xl font-bold">Hello World</h1>
-    </main>
-  )
-}
