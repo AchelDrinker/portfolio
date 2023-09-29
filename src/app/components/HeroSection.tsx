319f254ac82d1162afcfbe1d321e4ec72ad2ea10
Hugo Martineu <hugo.martineu@gmail.com>
2023-09-29 09:42:29 +0200

    start

---
+++ src/app/components/HeroSection.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+        <h1 className="text-white">Hello, I'm Hugo</h1>
+        <p className="text-[#ADB7BE]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum consectetur neque eu sem finibus, id faucibus erat suscipit.
+        </p>
+    </section>
+  )
+}
+
+export default HeroSection
