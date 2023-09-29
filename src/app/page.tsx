600aa501808d5e415bd220d1e45955e761108abc
Hugo Martineu <hugo.martineu@gmail.com>
2023-09-29 10:52:25 +0200

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import HeroSection from './components/HeroSection'
+import { Container } from 'postcss'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12 py-4">
-      <HeroSection />
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+      <div className="container mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
     </main>
   )
 }
