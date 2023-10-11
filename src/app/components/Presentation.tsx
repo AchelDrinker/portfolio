6f9b4fedb25ef7c550a0f80185d68c26b9a42746
Hugo Martineu <hugo.martineu@gmail.com>
2023-10-11 09:06:41 +0200

    download CV

--- src/app/components/Presentation.tsx
+++ src/app/components/Presentation.tsx
@@ -33,14 +33,18 @@ const Presentation = () => {
           I&apos;m currently retraining in the IT field and I&apos;m looking for a 4 to 6 months internship in Development, Data or Machine Learning
           </p>
           <div>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-r from-blue-600 via-indigo-500 to-green-400 hover:bg-slate-200 text-white">
-              Hire me
-            </button>
-            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-r from-blue-600 via-indigo-500 to-green-400 hover:bg-slate-800 text-white mt-3">
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-              Download CV
-              </span>
-            </button>
+            <Link href={"https://www.linkedin.com/in/hugomartineu"}>
+              <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-r from-blue-600 via-indigo-500 to-green-400 hover:bg-slate-200 text-white">
+                Hire me
+              </button>
+            </Link>
+            <a href="/elements/CV-HUGO-MARTINEU-STAGE.pdf" download>
+              <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-r from-blue-600 via-indigo-500 to-green-400 hover:bg-slate-800 text-white mt-3">
+                <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+                </span>
+              </button>
+            </a>
           </div>
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
