4fedcb3e1473be918f31980f9268c03c52e65edc
Hugo Martineu <hugo.martineu@gmail.com>
2023-10-11 10:03:25 +0200

    resolution

--- src/app/components/Presentation.tsx
+++ src/app/components/Presentation.tsx
@@ -8,7 +8,7 @@ const Presentation = () => {
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold min-h-[4em]">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold min-h-[3.5em]">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-600 via-indigo-500 to-green-400">
               Hello, I&apos;m {" "}
             </span>
