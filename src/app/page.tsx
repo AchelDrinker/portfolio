7fa3d1a06987bbdd99678593d6b666a9670cfb98
Hugo Martineu <hugo.martineu@gmail.com>
2023-10-14 11:28:58 +0200

    header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,20 +10,28 @@ import AchievementsSection from "./components/AchievementsSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <>
       <Head>
-        <title>Hugo Martineu</title>
+        <title>Hugo Martineu - Portfolio</title>
         <link rel="icon" type="image/jpg" href="/images/LogoSteam.jpg" />
+        <meta name="description" content="Portfolio de Hugo Martineu, développeur et data scientist." />
+        <meta name="keywords" content="Hugo Martineu, Data Science, Développement Web, Portfolio" />
       </Head>
-      <NavBar />
-      <div className="container mt-24 mx-auto px-12 py-4">
-        <Presentation />
-        <AchievementsSection />
-        <AboutSection />
-        <ProjectsSection />
-        <EmailSection />
-      </div>
-      <Footer />
-    </main>
+      <header>
+        <NavBar />
+      </header>
+      <main className="flex min-h-screen flex-col bg-[#121212]">
+        <section className="container mt-24 mx-auto px-12 py-4">
+            <Presentation />
+            <AchievementsSection />
+            <AboutSection />
+            <ProjectsSection />
+            <EmailSection />
+        </section>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
   );
 }
