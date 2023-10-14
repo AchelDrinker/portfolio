0b2f9dcd839a8c009292975fdc376d084982c840
Hugo Martineu <hugo.martineu@gmail.com>
2023-10-14 11:13:04 +0200

    header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Image from "next/image";
 import Presentation from "./components/Presentation";
 import NavBar from "./components/NavBar";
@@ -10,6 +11,10 @@ import AchievementsSection from "./components/AchievementsSection";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+      <Head>
+        <title>Hugo Martineu</title>
+        <link rel="icon" type="image/jpg" href="/images/LogoSteam.jpg" />
+      </Head>
       <NavBar />
       <div className="container mt-24 mx-auto px-12 py-4">
         <Presentation />
